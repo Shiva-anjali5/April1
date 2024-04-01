d8ff72e2b5afeedf4fdd0b84d0945c806e1bf9c8
Shiva-anjali5 <128413700+Shiva-anjali5@users.noreply.github.com>
2024-04-01 19:22:56 +0530

    first commit

---
+++ JavaCiCd.java
@@ -0,0 +1,8 @@
+public class JavaCiCd{
+    public static void main(String args[])
+    {
+        System.out.println("************************************");
+        System.out.println("Just for Testing Integration and diployment");
+
+    }
+}
---
+++ dockerfile
@@ -0,0 +1,5 @@
+From openjdk:8
+COPY . /SRC/JAVA
+WORKDIR /SRC/JAVA
+RUN ["javac","JavaCiCd.java"]
+ENTRYPOINT ['java', 'JavaCiCd']
